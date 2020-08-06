dd0e274274a85dbe3d5f41935964a677b4a39814
hds536jhmk <hds536jhmk@gmail.com>
2020-08-06 02:05:39 +0200

    Made a generic Conduit class to make Conduit creation easier

---
+++ src/main/java/com/hds/testmod/blocks/conduits/ModConduit.java
@@ -0,0 +1,87 @@
+package com.hds.testmod.blocks.conduits;
+
+import com.hds.testmod.util.customclasses.DirectionToBooleanProperty;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+
+public abstract class ModConduit extends Block {
+
+    public ModConduit(Properties properties) {
+        super(properties);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        VoxelShape conduitShape = Block.makeCuboidShape(6, 6, 6, 10, 10, 10);
+
+        ArrayList<VoxelShape> shapesToAdd = new ArrayList<>();
+        for (Direction direction : Direction.values()) {
+            boolean hasConnection = state.get(DirectionToBooleanProperty.get(direction));
+            if (!hasConnection)
+                continue;
+
+            Vec3i directionVector = direction.getDirectionVec();
+            shapesToAdd.add(Block.makeCuboidShape(
+                    7,
+                    7,
+                    7,
+                    directionVector.getX() == 0 ? 9 : (directionVector.getX() > 0 ? 16 : 0),
+                    directionVector.getY() == 0 ? 9 : (directionVector.getY() > 0 ? 16 : 0),
+                    directionVector.getZ() == 0 ? 9 : (directionVector.getZ() > 0 ? 16 : 0)
+            ));
+        }
+
+        for (VoxelShape voxelShape : shapesToAdd) {
+            conduitShape = VoxelShapes.combine(conduitShape, voxelShape, IBooleanFunction.OR);
+        }
+
+        return conduitShape.simplify();
+    }
+
+    @Override
+    public abstract boolean hasTileEntity(BlockState state);
+
+    @Nullable
+    @Override
+    public abstract TileEntity createTileEntity(BlockState state, IBlockReader world);
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState()
+                .with(BlockStateProperties.WEST, false)
+                .with(BlockStateProperties.UP, false)
+                .with(BlockStateProperties.SOUTH, false)
+                .with(BlockStateProperties.NORTH, false)
+                .with(BlockStateProperties.EAST, false)
+                .with(BlockStateProperties.DOWN, false);
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(
+                BlockStateProperties.WEST,
+                BlockStateProperties.UP,
+                BlockStateProperties.SOUTH,
+                BlockStateProperties.NORTH,
+                BlockStateProperties.EAST,
+                BlockStateProperties.DOWN
+        );
+    }
+}
--- src/main/java/com/hds/testmod/blocks/conduits/sapphire/SapphireConduitTile.java
+++ src/main/java/com/hds/testmod/blocks/conduits/sapphire/SapphireConduitTile.java
@@ -1,89 +1,10 @@
 package com.hds.testmod.blocks.conduits.sapphire;
 
+import com.hds.testmod.blocks.conduits.ModConduitTile;
 import com.hds.testmod.registries.ModTileEntities;
-import com.hds.testmod.util.customclasses.DirectionToBooleanProperty;
-import com.hds.testmod.util.customclasses.ModEnergyStorage;
-import net.minecraft.block.BlockState;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.state.BooleanProperty;
-import net.minecraft.tileentity.ITickableTileEntity;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.energy.CapabilityEnergy;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.util.concurrent.atomic.AtomicReference;
-
-public class SapphireConduitTile extends TileEntity implements ITickableTileEntity {
-    private static final int MAX_INPUT = 100;
-    private static final int MAX_OUTPUT = 100;
-    private static final int CAPACITY = 200;
-
-    private final ModEnergyStorage CONDUIT_ENERGY_BUFFER = new ModEnergyStorage(CAPACITY, MAX_INPUT, MAX_OUTPUT, 0) {
-        @Override
-        public void onEnergyChanged(int energyAdded) {
-            markDirty();
-        }
-    };
-
+public class SapphireConduitTile extends ModConduitTile {
     public SapphireConduitTile() {
-        super(ModTileEntities.SAPPHIRE_CONDUIT_TILE.get());
-    }
-
-    @Override
-    public void tick() {
-        if (world.isRemote)
-            return;
-        BlockPos thisPos = this.getPos();
-        AtomicReference<BlockState> thisBlockState = new AtomicReference<>(world.getBlockState(thisPos));
-        for (Direction direction : Direction.values()) {
-            BooleanProperty attachedFace = DirectionToBooleanProperty.get(direction);
-            thisBlockState.set(thisBlockState.get().with(attachedFace, false));
-            TileEntity tileEntity = world.getTileEntity(thisPos.offset(direction));
-            if (tileEntity == null)
-                continue;
-            tileEntity.getCapability(CapabilityEnergy.ENERGY, direction.getOpposite()).ifPresent(
-                    energyHandler -> {
-                        thisBlockState.set(thisBlockState.get().with(attachedFace, true));
-                        if (energyHandler.canReceive() && (energyHandler.getEnergyStored() < CONDUIT_ENERGY_BUFFER.getEnergyStored() || CONDUIT_ENERGY_BUFFER.getEnergyStored() >= CONDUIT_ENERGY_BUFFER.getMaxEnergyStored())) {
-                            int receivedEnergy = energyHandler.receiveEnergy(MAX_OUTPUT, true);
-                            int takenEnergy = CONDUIT_ENERGY_BUFFER.extractEnergy(receivedEnergy, false);
-                            energyHandler.receiveEnergy(takenEnergy, false);
-
-                        } else if (energyHandler.canExtract()) {
-                            int takenEnergy = energyHandler.extractEnergy(MAX_INPUT, true);
-                            int receivedEnergy = CONDUIT_ENERGY_BUFFER.receiveEnergy(takenEnergy, false);
-                            energyHandler.extractEnergy(receivedEnergy, false);
-
-                        }
-                    }
-            );
-        }
-        world.setBlockState(thisPos, thisBlockState.get());
-    }
-
-    @Override
-    public void read(CompoundNBT compound) {
-        super.read(compound);
-        CONDUIT_ENERGY_BUFFER.deserializeNBT(compound.getCompound("energyBuffer"));
-    }
-
-    @Override
-    public CompoundNBT write(CompoundNBT compound) {
-        compound.put("energyBuffer", CONDUIT_ENERGY_BUFFER.serializeNBT());
-        return super.write(compound);
-    }
-
-    @Nonnull
-    @Override
-    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-        if (cap.equals(CapabilityEnergy.ENERGY)) {
-            return LazyOptional.of(() -> CONDUIT_ENERGY_BUFFER).cast();
-        }
-        return super.getCapability(cap, side);
+        super(ModTileEntities.SAPPHIRE_CONDUIT_TILE.get(), 100, 100, 200);
     }
 }
