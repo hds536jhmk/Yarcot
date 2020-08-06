8707ee2153b5558b26c76200de89afed9da0f006
hds536jhmk <hds536jhmk@gmail.com>
2020-08-06 03:20:34 +0200

    Made a GUI for Sapphire Battery

--- src/main/java/com/hds/testmod/blocks/batteries/sapphire/SapphireBatteryScreen.java
+++ src/main/java/com/hds/testmod/blocks/batteries/sapphire/SapphireBatteryScreen.java
@@ -2,8 +2,6 @@ package com.hds.testmod.blocks.batteries.sapphire;
 
 import com.hds.testmod.TestMod;
 import com.hds.testmod.blocks.barrels.ModBarrelContainer;
-import com.hds.testmod.blocks.barrels.sapphire.SapphireBarrelContainer;
-import com.hds.testmod.blocks.barrels.sapphire.SapphireBarrelTile;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
@@ -12,10 +10,11 @@ import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+import java.text.DecimalFormat;
+
 @OnlyIn(Dist.CLIENT)
 public class SapphireBatteryScreen extends ContainerScreen<SapphireBatteryContainer> {
-    // TODO: Make an actual GUI
-    private final ResourceLocation GUI_TEXTURE = new ResourceLocation(TestMod.MOD_ID, "textures/gui/container/sapphire_barrel.png");
+    private final ResourceLocation GUI_TEXTURE = new ResourceLocation(TestMod.MOD_ID, "textures/gui/container/sapphire_battery.png");
 
     public SapphireBatteryScreen(SapphireBatteryContainer screenContainer, PlayerInventory inv, ITextComponent titleIn) {
         super(screenContainer, inv, titleIn);
@@ -23,8 +22,8 @@ public class SapphireBatteryScreen extends ContainerScreen<SapphireBatteryContai
 
     @Override
     protected void init() {
-        this.xSize = 14 + 11 * ModBarrelContainer.CELL_SIZE;
-        this.ySize = 40 + 9 * ModBarrelContainer.CELL_SIZE;
+        this.xSize = 14 + 9 * ModBarrelContainer.CELL_SIZE;
+        this.ySize = 40 + 7 * ModBarrelContainer.CELL_SIZE;
         super.init();
     }
 
@@ -37,11 +36,16 @@ public class SapphireBatteryScreen extends ContainerScreen<SapphireBatteryContai
 
     @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
-        this.font.drawString(Integer.toString(this.container.getEnergy()), 8.0F, 6.0F, 0x404040);
+        this.font.drawString(this.title.getFormattedText(), 8.0F, 6.0F, 0x404040);
+
+        String batteryCharge = DecimalFormat.getPercentInstance().format(this.container.BATTERY_TILE.getChargePercentage());
+        int stringWidth = this.font.getStringWidth(batteryCharge);
+        this.font.drawString(batteryCharge, 144 - stringWidth / 2, 72, 0x404040);
+
         this.font.drawString(
                 this.playerInventory.getDisplayName().getFormattedText(),
                 8.0F,
-                19 + 5 * ModBarrelContainer.CELL_SIZE,
+                19 + 3 * ModBarrelContainer.CELL_SIZE,
                 0x404040
         );
     }
@@ -51,5 +55,6 @@ public class SapphireBatteryScreen extends ContainerScreen<SapphireBatteryContai
         RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.minecraft.getTextureManager().bindTexture(GUI_TEXTURE);
         this.blit(this.getGuiLeft(), this.getGuiTop(), 0, 0, this.xSize, this.ySize);
+        this.blit(this.getGuiLeft() + 136, this.getGuiTop() + 57, 186, 53, 16, (int)(this.container.BATTERY_TILE.getChargePercentage() * -32.0F));
     }
 }
