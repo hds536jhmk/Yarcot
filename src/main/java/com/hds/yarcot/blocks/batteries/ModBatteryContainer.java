720391736075999fd7e3cbd6ee27153052f0bd90
hds536jhmk <hds536jhmk@gmail.com>
2020-08-08 01:30:10 +0200

    Added ModEnergyContainer and made ModBatteryContainer extend it

--- src/main/java/com/hds/yarcot/blocks/batteries/ModBatteryContainer.java
+++ src/main/java/com/hds/yarcot/blocks/batteries/ModBatteryContainer.java
@@ -1,24 +1,21 @@
 package com.hds.yarcot.blocks.batteries;
 
 import com.hds.yarcot.util.ModLog;
-import com.hds.yarcot.util.customclasses.ModEnergyStorage;
-import com.hds.yarcot.util.customclasses.ModInventoryContainer;
+import com.hds.yarcot.util.customclasses.ModEnergyContainer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.ContainerType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.IntReferenceHolder;
 import net.minecraft.util.math.BlockPos;
-import net.minecraftforge.energy.CapabilityEnergy;
 
 import javax.annotation.Nullable;
 
-public abstract class ModBatteryContainer extends ModInventoryContainer {
+public abstract class ModBatteryContainer extends ModEnergyContainer {
     protected final ModBatteryTile BATTERY_TILE;
 
     public ModBatteryContainer(@Nullable ContainerType<?> type, int windowId, PlayerInventory playerInventory, BlockPos pos) {
-        super(type, windowId, playerInventory);
+        super(type, windowId, playerInventory, playerInventory.player.world.getTileEntity(pos));
 
         TileEntity te = playerInventory.player.world.getTileEntity(pos);
         if (!(te instanceof ModBatteryTile))
@@ -27,41 +24,6 @@ public abstract class ModBatteryContainer extends ModInventoryContainer {
         this.BATTERY_TILE = (ModBatteryTile) te;
 
         this.trackEnergy();
-    }
-
-    public void trackEnergy() {
-        trackInt(new IntReferenceHolder() {
-            @Override
-            // This is ran on the Server to get the energy from the tile entity
-            public int get() {
-                // The returned value gets sent to the client
-                return BATTERY_TILE.getCharge() & 0xffff;
-            }
-
-            @Override
-            // This is ran on the client to set values on the tileentity that's stored on the client
-            public void set(int serverEnergy) {
-                // Sets battery's energy to be the same as the server's
-                BATTERY_TILE.getCapability(CapabilityEnergy.ENERGY).ifPresent(energyStorageHandler -> {
-                    int energyStored = energyStorageHandler.getEnergyStored() & 0xffff0000;
-                    ((ModEnergyStorage)energyStorageHandler).setEnergy(energyStored + (serverEnergy & 0xffff));
-                });
-            }
-        });
-        trackInt(new IntReferenceHolder() {
-            @Override
-            public int get() {
-                return (BATTERY_TILE.getCharge() >> 16) & 0xffff;
-            }
-
-            @Override
-            public void set(int serverEnergy) {
-                BATTERY_TILE.getCapability(CapabilityEnergy.ENERGY).ifPresent(energyStorageHandler -> {
-                    int energyStored = energyStorageHandler.getEnergyStored() & 0x0000ffff;
-                    ((ModEnergyStorage)energyStorageHandler).setEnergy(energyStored | (serverEnergy << 16));
-                });
-            }
-        });
     }
 
     @Override
---
+++ src/main/java/com/hds/yarcot/util/customclasses/ModEnergyContainer.java
@@ -0,0 +1,54 @@
+package com.hds.yarcot.util.customclasses;
+
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IntReferenceHolder;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.energy.IEnergyStorage;
+
+import javax.annotation.Nullable;
+
+public abstract class ModEnergyContainer extends ModInventoryContainer {
+    protected final TileEntity CONTAINER_TILE_ENTITY;
+
+    public ModEnergyContainer(@Nullable ContainerType<?> type, int windowId, PlayerInventory playerInventory, TileEntity tileEntity) {
+        super(type, windowId, playerInventory);
+        CONTAINER_TILE_ENTITY = tileEntity;
+    }
+
+    protected void trackEnergy() {
+        trackInt(new IntReferenceHolder() {
+            @Override
+            // This is ran on the Server to get the energy from the tile entity
+            public int get() {
+                // The returned value gets sent to the client
+                return CONTAINER_TILE_ENTITY.getCapability(CapabilityEnergy.ENERGY).map(IEnergyStorage::getEnergyStored).orElse(0) & 0xffff;
+            }
+
+            @Override
+            // This is ran on the client to set values on the tileentity that's stored on the client
+            public void set(int serverEnergy) {
+                // Sets battery's energy to be the same as the server's
+                CONTAINER_TILE_ENTITY.getCapability(CapabilityEnergy.ENERGY).ifPresent(energyStorageHandler -> {
+                    int energyStored = energyStorageHandler.getEnergyStored() & 0xffff0000;
+                    ((ModEnergyStorage)energyStorageHandler).setEnergy(energyStored + (serverEnergy & 0xffff));
+                });
+            }
+        });
+        trackInt(new IntReferenceHolder() {
+            @Override
+            public int get() {
+                return (CONTAINER_TILE_ENTITY.getCapability(CapabilityEnergy.ENERGY).map(IEnergyStorage::getEnergyStored).orElse(0) >> 16) & 0xffff;
+            }
+
+            @Override
+            public void set(int serverEnergy) {
+                CONTAINER_TILE_ENTITY.getCapability(CapabilityEnergy.ENERGY).ifPresent(energyStorageHandler -> {
+                    int energyStored = energyStorageHandler.getEnergyStored() & 0x0000ffff;
+                    ((ModEnergyStorage)energyStorageHandler).setEnergy(energyStored | (serverEnergy << 16));
+                });
+            }
+        });
+    }
+}
