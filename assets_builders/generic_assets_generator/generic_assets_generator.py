65744470c78582e5220b19dbd3e110a8d77b3dc7
hds536jhmk <hds536jhmk@gmail.com>
2020-08-18 17:10:59 +0200

    Removed unused code

--- assets_builders/generic_assets_generator/generic_assets_generator.py
+++ assets_builders/generic_assets_generator/generic_assets_generator.py
@@ -9,17 +9,6 @@ from PIL import Image, ImageOps
 from termcolor import cprint, COLORS
 
 
-class TerminalColors:
-    HEADER    = "\033[95m"
-    OKBLUE    = "\033[94m"
-    OKGREEN   = "\033[92m"
-    WARNING   = "\033[93m"
-    FAIL      = "\033[91m"
-    ENDC      = "\033[0m"
-    BOLD      = "\033[1m"
-    UNDERLINE = "\033[4m"
-
-
 class Settings:
     def __init__(self, outputPath: str, texturesPath: str, colorMode: str, fileFormat: str):
         self.outputPath = outputPath
