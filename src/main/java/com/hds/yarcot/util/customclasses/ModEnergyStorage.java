8afd595862558574c2fc08e3a1f87dbcdb97f4d7
hds536jhmk <hds536jhmk@gmail.com>
2020-08-19 21:32:24 +0200

    ModEnergyStorage doesn't extend EnergyStorage anymore but it implements IEnergyStorage instead

--- src/main/java/com/hds/yarcot/util/customclasses/ModEnergyStorage.java
+++ src/main/java/com/hds/yarcot/util/customclasses/ModEnergyStorage.java
@@ -2,9 +2,14 @@ package com.hds.yarcot.util.customclasses;
 
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.common.util.INBTSerializable;
-import net.minecraftforge.energy.EnergyStorage;
+import net.minecraftforge.energy.IEnergyStorage;
 
-public abstract class ModEnergyStorage extends EnergyStorage implements INBTSerializable<CompoundNBT> {
+public class ModEnergyStorage implements IEnergyStorage, INBTSerializable<CompoundNBT> {
+
+    private int energy;
+    private int capacity;
+    protected int maxReceive;
+    protected int maxExtract;
 
     public ModEnergyStorage(int capacity) {
         this(capacity, 0, 0, 0);
@@ -19,31 +24,22 @@ public abstract class ModEnergyStorage extends EnergyStorage implements INBTSeri
     }
 
     public ModEnergyStorage(int capacity, int maxReceive, int maxExtract, int energy) {
-        super(capacity, maxReceive, maxExtract, energy);
+        this.capacity = capacity;
+        this.maxReceive = maxReceive;
+        this.maxExtract = maxExtract;
+        this.energy = Math.max(0 , Math.min(capacity, energy));
     }
 
-    public abstract void onEnergyChanged(int energyAdded);
+    public void onEnergyChanged(int energyAdded) {}
+
+    public void setEnergyRaw(int energy) {
+        this.energy = Math.max(Math.min(energy, this.getMaxEnergyStored()), 0);
+    }
 
     public void setEnergy(int energy) {
-        int initialEnergy = this.energy;
-        this.energy = Math.max(Math.min(energy, this.capacity), 0);
-        onEnergyChanged(this.energy - initialEnergy);
-    }
-
-    @Override
-    public int receiveEnergy(int maxReceive, boolean simulate) {
-        int receivedEnergy = super.receiveEnergy(maxReceive, simulate);
-        if (!simulate)
-            onEnergyChanged(receivedEnergy);
-        return receivedEnergy;
-    }
-
-    @Override
-    public int extractEnergy(int maxExtract, boolean simulate) {
-        int extractedEnergy = super.extractEnergy(maxExtract, simulate);
-        if (!simulate)
-            onEnergyChanged(-extractedEnergy);
-        return extractedEnergy;
+        int initialEnergy = this.getEnergyStored();
+        this.setEnergyRaw(energy);
+        onEnergyChanged(this.getEnergyStored() - initialEnergy);
     }
 
     public int consumeEnergy(int maxExtract, boolean simulate) {
@@ -55,15 +51,76 @@ public abstract class ModEnergyStorage extends EnergyStorage implements INBTSeri
         return extractedEnergy;
     }
 
+    public static int transferEnergy(IEnergyStorage from, IEnergyStorage to, int energy) {
+        int extractableEnergy = from.extractEnergy(energy, true);
+        int energyExtracted = to.receiveEnergy(extractableEnergy, false);
+        from.extractEnergy(energyExtracted, false);
+        return energyExtracted;
+    }
+
+    public static void copyEnergy(IEnergyStorage from, IEnergyStorage to) {
+        ((ModEnergyStorage) to).setEnergy(from.getEnergyStored());
+    }
+
+    @Override
+    public int receiveEnergy(int maxReceive, boolean simulate) {
+        int energyReceived = 0;
+
+        if (canReceive())
+            energyReceived = Math.min(this.getMaxEnergyStored() - this.getEnergyStored(), Math.min(this.maxReceive, maxReceive));
+
+        if (!simulate)
+            this.setEnergy(this.getEnergyStored() + energyReceived);
+
+        return energyReceived;
+    }
+
+    @Override
+    public int extractEnergy(int maxExtract, boolean simulate) {
+        int energyExtracted = 0;
+
+        if (canExtract())
+            energyExtracted = Math.min(this.getEnergyStored(), Math.min(this.maxExtract, maxExtract));
+
+        if (!simulate)
+            this.setEnergy(this.getEnergyStored() - energyExtracted);
+
+        return energyExtracted;
+    }
+
+    @Override
+    public int getEnergyStored()
+    {
+        return this.energy;
+    }
+
+    @Override
+    public int getMaxEnergyStored()
+    {
+        return this.capacity;
+    }
+
+    @Override
+    public boolean canExtract()
+    {
+        return this.maxExtract > 0;
+    }
+
+    @Override
+    public boolean canReceive()
+    {
+        return this.maxReceive > 0;
+    }
+
     @Override
     public CompoundNBT serializeNBT() {
         CompoundNBT nbt = new CompoundNBT();
-        nbt.putInt("energy", this.energy);
+        nbt.putInt("energy", this.getEnergyStored());
         return nbt;
     }
 
     @Override
     public void deserializeNBT(CompoundNBT nbt) {
-        this.energy = nbt.getInt("energy");
+        this.setEnergy(nbt.getInt("energy"));
     }
 }
