9ddecc53eca155d14cc2054113f446f25219abf6
hds536jhmk <hds536jhmk@gmail.com>
2020-08-20 04:11:20 +0200

    Updated ModFurnaceInventory to take advantage of ItemHandlerInventory

--- src/main/java/com/hds/yarcot/blocks/furnaces/ModFurnaceInventory.java
+++ src/main/java/com/hds/yarcot/blocks/furnaces/ModFurnaceInventory.java
@@ -1,5 +1,6 @@
 package com.hds.yarcot.blocks.furnaces;
 
+import com.hds.yarcot.util.customclasses.ItemHandlerInventory;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
@@ -10,16 +11,16 @@ import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nullable;
 
-public abstract class ModFurnaceInventory implements ISidedInventory, INBTSerializable<CompoundNBT> {
-    private final ItemStackHandler ITEM_STACK_HANDLER = new ItemStackHandler(2) {
-        @Override
-        protected void onContentsChanged(int slot) {
-            markDirty();
-        }
-    };
+public abstract class ModFurnaceInventory extends ItemHandlerInventory implements ISidedInventory, INBTSerializable<CompoundNBT> {
 
-    public ItemStackHandler getItemStackHandler() {
-        return ITEM_STACK_HANDLER;
+    public ModFurnaceInventory() {
+        super(true);
+        this.itemHandler = new ItemStackHandler(2) {
+            @Override
+            protected void onContentsChanged(int slot) {
+                markDirty();
+            }
+        };
     }
 
     @Override
@@ -42,47 +43,6 @@ public abstract class ModFurnaceInventory implements ISidedInventory, INBTSerial
     }
 
     @Override
-    public int getSizeInventory() {
-        return ITEM_STACK_HANDLER.getSlots();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        for (int i = 0; i < ITEM_STACK_HANDLER.getSlots(); i++) {
-            if (!ITEM_STACK_HANDLER.getStackInSlot(i).isEmpty())
-                return false;
-        }
-        return true;
-    }
-
-    @Override
-    public ItemStack getStackInSlot(int index) {
-        return ITEM_STACK_HANDLER.getStackInSlot(index);
-    }
-
-    @Override
-    public ItemStack decrStackSize(int index, int count) {
-        ItemStack slotToModify = ITEM_STACK_HANDLER.getStackInSlot(index);
-        ItemStack removed = slotToModify.copy();
-        removed.setCount(count);
-        slotToModify.setCount(slotToModify.getCount() - count);
-        return removed;
-    }
-
-    @Override
-    public ItemStack removeStackFromSlot(int index) {
-        ItemStack slotToModify = ITEM_STACK_HANDLER.getStackInSlot(index);
-        ItemStack oldStack = slotToModify.copy();
-        slotToModify.setCount(0);
-        return oldStack;
-    }
-
-    @Override
-    public void setInventorySlotContents(int index, ItemStack stack) {
-        ITEM_STACK_HANDLER.setStackInSlot(index, stack);
-    }
-
-    @Override
     public abstract void markDirty();
 
     @Override
@@ -91,20 +51,14 @@ public abstract class ModFurnaceInventory implements ISidedInventory, INBTSerial
     }
 
     @Override
-    public void clear() {
-        for (int i = 0; i < ITEM_STACK_HANDLER.getSlots(); i++)
-            ITEM_STACK_HANDLER.getStackInSlot(i).setCount(0);
-    }
-
-    @Override
     public CompoundNBT serializeNBT() {
         CompoundNBT nbt = new CompoundNBT();
-        nbt.put("itemStackHandler", ITEM_STACK_HANDLER.serializeNBT());
+        nbt.put("itemStackHandler", itemHandler.serializeNBT());
         return nbt;
     }
 
     @Override
     public void deserializeNBT(CompoundNBT nbt) {
-        ITEM_STACK_HANDLER.deserializeNBT(nbt.getCompound("itemStackHandler"));
+        itemHandler.deserializeNBT(nbt.getCompound("itemStackHandler"));
     }
 }
