59328cfe56cbebe411107f9b6d04e2d43fa4ddf0
hds536jhmk <hds536jhmk@gmail.com>
2020-08-24 01:01:19 +0200

    Renamed ModEvents to ModEventBus

---
+++ src/main/java/com/hds/yarcot/util/ModEventBus.java
@@ -0,0 +1,15 @@
+package com.hds.yarcot.util;
+
+import com.hds.yarcot.Yarcot;
+import com.hds.yarcot.world.ModOreGen;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod.EventBusSubscriber(modid = Yarcot.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventBus {
+    @SubscribeEvent
+    public static void onLoadComplete(FMLLoadCompleteEvent event) {
+        ModOreGen.registerOres();
+    }
+}
